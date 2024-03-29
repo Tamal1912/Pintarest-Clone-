e07beeb3e4527df8e0542d5e832142eceb429cf0
Tamal Sur <tamalsur14@gmail.com>
2024-03-29 14:18:58 +0530

    third commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
+const userModel=require("./users");
+const passport = require('passport');
+const localStrategy=require("passport-local")
+
+passport.use(new localStrategy(userModel.authenticate()));
+
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+router.get('/profile',isLoggedin , function(req, res, next) {
+  res.render('profile');
 });
 
 router.get('/register', function(req, res, next) {
   res.render('register');
 });
 
+router.post('/register', function(req, res, next) {
+ let data=new userModel({
+  username:req.body.username,
+  name:req.body.name,
+  email:req.body.email
+ });
+
+ userModel.register(data,req.body.password)
+ .then(()=>{
+  passport.authenticate("local"),(req,res,function(){
+    res.redirect("/profile");
+  })
+ })
+});
+
+router.post('/login', passport.authenticate("local",{
+  successRedirect:"/profile",
+  failureRedirect:"/"
+}),function(req, res, next) {
+});
+
+router.get("/logout", isLoggedin,(req,res)=>{
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/login');
+  });
+})
+
+function isLoggedin(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+
+  res.redirect("/login");
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const mongoose=require("mongoose");
+const passport = require("passport");
+const plm=require("passport-local-mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
+
+
+const userModel=mongoose.Schema({
+  username:String,
+  name:String,
+  email:String,
+  password:String,
+  profileImage:String,
+  boards:[] 
+})
+
+userModel.plugin(plm);
+
+module.exports=mongoose.model("user",userModel);
